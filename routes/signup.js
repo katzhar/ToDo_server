3bef9cf9260d983d4a4216e4613daeb31f4e59d4
zharovova <zharovovaea@gmail.com>
2020-08-18 14:01:23 +0300

    validation

--- routes/signup.js
+++ routes/signup.js
@@ -11,13 +11,14 @@ router.post('/', [
     check('username')
         .notEmpty()
         .withMessage('username is required')
-        .isLength({ min: 5 }),
+        .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/)
+        .withMessage('username should not be empty, minimum six characters, at least one letter and one number'),
     check('email')
         .isEmail()
         .withMessage('invalid email address'),
     check('password')
-        .isLength({ min: 6 })
-        .withMessage('password is required')
+        .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/)
+        .withMessage('password should not be empty, minimum six characters, at least one letter and one number')
 ], (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty())
@@ -40,52 +41,5 @@ router.post('/', [
             });
     }
 });
-
-module.exports = router;
-
-
-// require('dotenv/config');
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const Users = require('../models/users');
-
-// const router = express.Router();
-// router.use(bodyParser.json());
-
-// router.post('/', (req, res) => {
-
-//     const emailChecker = email => {
-//         const regex = /\S+@\S+\.\S+/;
-//         return regex.test(email);
-//     };
-
-//     const pwdChecker = password => {
-//         const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/;
-//         return regex.test(password);
-//     };
-
-//     async function validation(data) {
-//         if (await Users.exists({ email: data.email }))
-//             return res.send({ msg: 'this email is already in use' });
-//         if (!emailChecker(data.email))
-//             return res.send({ msg: 'incorrect format of email' });
-//         if (await Users.exists({ login: data.login }))
-//             return res.send({ msg: 'this login is already in use' });
-//         if (!pwdChecker(data.password))
-//             return res.send({ msg: 'Your password must be at least 6 characters long, be of mixed case and also contain a digit or symbol.' })
-//         else {
-//             let user = new Users({
-//                 fn: data.fn,
-//                 ln: data.ln,
-//                 login: data.login,
-//                 email: data.email,
-//                 password: data.password,
-//                 confirmlink: token
-//             })
-//             user.save();
-//         }
-//     }
-//     validation(req.body);
-// });
 
 module.exports = router;
