f0de7decd9133fb2bc6b4f583c5d48f0a5f3820e
zharovova <zharovovaea@gmail.com>
2020-08-20 10:00:32 +0300

    logout

--- routes/logout.js
+++ routes/logout.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require('express-jwt');
 const authenticate = require('../authenticate');
 
 const router = express.Router();
 router.use(bodyParser.json());
 
 router.get('/', authenticate.verifyUser, (req, res) => {
-  // const token = req.headers.authorization.split(' ')[1];
   res.statusCode = 200;
   res.json({ success: true });
 });
