d8629a69567b9951a6109d99a1ee7af7311bfc72
Грязева Анастасия Анатольевна <AAnGryazeva@sberbank.ru>
2020-08-26 11:15:56 +0300

    ok

--- routes/todolist.js
+++ routes/todolist.js
@@ -1,16 +1,16 @@
-const { Router } = require('express');
+const {Router} = require('express');
 const router = Router();
 const authenticate = require('../authenticate');
 const asyncHandler = require('express-async-handler');
 const Users = require('../models/users');
-const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 
 router.get('/', authenticate.verifyUser, asyncHandler(async (req, res) => {
 	const token = req.headers.authorization.split(' ')[1];
 	const isTokenBlacklisted = await authenticate.isBlacklisted(token);
 	if (isTokenBlacklisted)
 		return res.status(401).send('Token is invalid');
-	const todo = await Users.findOne({ _id: req.user._id }, 'tasks');
+	const todo = await Users.findOne({_id: req.user._id}, 'tasks');
 	return res.status(200).send(todo || []);
 }));
 
@@ -21,14 +21,18 @@ router.delete('/:id', authenticate.verifyUser, asyncHandler(async (req, res) => 
 		return res.status(401).send('Token is invalid');
 	else {
 		let taskId = req.params["id"];
-		await Users.updateOne({ _id: req.user._id }, { $pull: { "tasks": { id: taskId } } },
-			(err, result) => {
-				if (err)
-					res.status(500).send('Error');
-				else if (!result)
-					res.status(400).send('Task not found');
-			})
-		res.status(200).send('success');
+		let task = await Users.findOne({_id: req.user._id}, {'tasks': {$elemMatch: {'id': taskId}}});
+		if (!task.tasks[0])
+			return res.status(400).send('Id not found');
+		else {
+			await Users.updateOne({_id: req.user._id}, {$pull: {"tasks": {id: taskId}}},
+				(err, result) => {
+					if (err)
+						res.status(500).send('Error');
+					else
+						res.status(200).send('Success');
+				})
+		}
 	}
 }));
 
@@ -37,6 +41,8 @@ router.post('/', authenticate.verifyUser, asyncHandler(async (req, res) => {
 	const isTokenBlacklisted = await authenticate.isBlacklisted(token);
 	if (isTokenBlacklisted)
 		return res.status(401).send('Token is invalid');
+	if (typeof req.body.title !== "string" || typeof req.body.type !== "string" || typeof req.body.date !== "string")
+		return res.status(400).send('Wrong typeof title/type/date');
 	else {
 		let data = {
 			id: uuidv4(),
@@ -46,15 +52,16 @@ router.post('/', authenticate.verifyUser, asyncHandler(async (req, res) => {
 			description: req.body.description || '',
 			completed: false
 		}
-		await Users.updateOne({ _id: req.user._id },
-			{ $addToSet: { tasks: data } },
+		await Users.updateOne({_id: req.user._id},
+			{$addToSet: {tasks: data}},
 			(err, result) => {
 				if (err)
 					res.status(500).send('Error');
 				else if (!result)
 					res.status(400).send('Task not found');
+				else
+					res.status(200).send(data);
 			})
-		res.status(201).send(data);
 	}
 }));
 
@@ -65,38 +72,43 @@ router.put('/:id', authenticate.verifyUser, asyncHandler(async (req, res) => {
 		return res.status(401).send('Token is invalid');
 	else {
 		let taskId = req.params["id"];
-		let task = await Users.findOne({ _id: req.user._id }, { 'tasks': { $elemMatch: { 'id': taskId } } });
-		if (!task)
-			res.status(400).send('id not found');
-		let todo = task.tasks[0];
-		let todoKeys = Object.keys(req.body);
-		if (todoKeys.indexOf('title') !== -1)
-			todo.title = req.body.title;
-		if (todoKeys.indexOf('type') !== -1)
-			todo.type = req.body.type;
-		if (todoKeys.indexOf('date') !== -1)
-			todo.date = req.body.date;
-		if (todoKeys.indexOf('description') !== -1)
-			todo.description = req.body.description;
-		if (todoKeys.indexOf('completed') !== -1) {
-			todo.completed = req.body.completed;
+		let task = await Users.findOne({_id: req.user._id}, {'tasks': {$elemMatch: {'id': taskId}}});
+		if (!task.tasks[0])
+			return res.status(400).send('Id not found');
+		else {
+			let todo = task.tasks[0];
+			let todoKeys = Object.keys(req.body);
+			if (todoKeys.indexOf('title') !== -1)
+				todo.title = req.body.title;
+			if (todoKeys.indexOf('type') !== -1)
+				todo.type = req.body.type;
+			if (todoKeys.indexOf('date') !== -1)
+				todo.date = req.body.date;
+			if (todoKeys.indexOf('description') !== -1)
+				todo.description = req.body.description;
+			if (todoKeys.indexOf('completed') !== -1)
+				todo.completed = req.body.completed;
+			if(todoKeys.indexOf('title') === -1 && todoKeys.indexOf('type') === -1 && todoKeys.indexOf('date') === -1
+				&& todoKeys.indexOf('completed') === -1 && todoKeys.indexOf('description') === -1)
+				return res.status(400).send("Wrong param");
+			await Users.updateOne({_id: req.user._id, "tasks.id": taskId}, {
+					$set: {
+						"tasks.$.title": todo.title,
+						"tasks.$.description": todo.description,
+						"tasks.$.type": todo.type,
+						"tasks.$.date": todo.date,
+						"tasks.$.completed": todo.completed
+					}
+				},
+				(err, result) => {
+					if (err)
+						res.status(500).send('Error');
+					else if (!result)
+						res.status(400).send('Task not found');
+					else
+						res.status(200).send('Success');
+				})
 		}
-		await Users.updateOne({ _id: req.user._id, "tasks.id": taskId }, {
-			$set: {
-				"tasks.$.title": todo.title,
-				"tasks.$.description": todo.description,
-				"tasks.$.type": todo.type,
-				"tasks.$.date": todo.date,
-				"tasks.$.completed": todo.completed
-			}
-		},
-			(err, result) => {
-				if (err)
-					res.status(500).send('Error');
-				else if (!result)
-					res.status(400).send('Task not found');
-			})
-		res.status(200).send('success');
 	}
 }));
 
--- routes/types.js
+++ routes/types.js
@@ -1,17 +1,17 @@
-const { Router } = require('express');
+const {Router} = require('express');
 const router = Router();
 const authenticate = require('../authenticate');
 const asyncHandler = require('express-async-handler');
 const Users = require('../models/users');
-const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 
 router.get('/', authenticate.verifyUser, asyncHandler(async (req, res) => {
 	const token = req.headers.authorization.split(' ')[1];
 	const isTokenBlacklisted = await authenticate.isBlacklisted(token);
 	if (isTokenBlacklisted)
 		return res.status(401).send('Token is invalid');
-	const todo = await Users.findOne({ _id: req.user._id }, 'types');
-	res.status(200).send(todo || []);
+	const todo = await Users.findOne({_id: req.user._id}, 'types');
+	return res.status(200).send(todo || []);
 }));
 
 router.delete('/:id', authenticate.verifyUser, asyncHandler(async (req, res) => {
@@ -21,14 +21,18 @@ router.delete('/:id', authenticate.verifyUser, asyncHandler(async (req, res) => 
 		return res.status(401).send('Token is invalid');
 	else {
 		let typeId = req.params["id"];
-		await Users.updateOne({ _id: req.user._id }, { $pull: { "types": { id: typeId } } },
-			(err, result) => {
-				if (err)
-					res.status(500).send('Error');
-				else if (!result)
-					res.status(400).send('Type not found');
-			})
-		res.status(200).send('success');
+		let type = await Users.findOne({_id: req.user._id}, {'types': {$elemMatch: {'id': typeId}}});
+		if (!type.types[0])
+			return res.status(400).send('Id not found');
+		else {
+			await Users.updateOne({_id: req.user._id}, {$pull: {"types": {id: typeId}}},
+				(err, result) => {
+					if (err)
+						res.status(500).send('Error');
+					else
+						res.status(200).send('Success');
+				})
+		}
 	}
 }));
 
@@ -37,21 +41,22 @@ router.post('/', authenticate.verifyUser, asyncHandler(async (req, res) => {
 	const isTokenBlacklisted = await authenticate.isBlacklisted(token);
 	if (isTokenBlacklisted)
 		return res.status(401).send('Token is invalid');
+	if (typeof req.body.name !== "string")
+		return res.status(400).send('Wrong typeof name');
 	else {
 		let data = {
 			id: uuidv4(),
 			name: req.body.name,
 			colorId: req.body.colorId || '',
 		}
-		await Users.updateOne({ _id: req.user._id },
-			{ $addToSet: { types: data } },
+		await Users.updateOne({_id: req.user._id},
+			{$addToSet: {types: data}},
 			(err, result) => {
 				if (err)
 					res.status(500).send('Error');
-				else if (!result)
-					res.status(400).send('Error');
+				else
+					res.status(201).send(data);
 			})
-		res.status(201).send(data);
 	}
 }));
 
@@ -62,28 +67,33 @@ router.put('/:id', authenticate.verifyUser, asyncHandler(async (req, res) => {
 		return res.status(401).send('Token is invalid');
 	else {
 		let typeId = req.params["id"];
-		let types = await Users.findOne({ _id: req.user._id }, { 'types': { $elemMatch: { 'id': typeId } } });
-		if (!types)
-			res.status(400).send('id not found');
-		let type = types.types[0];
-		let typeKeys = Object.keys(req.body);
-		if (typeKeys.indexOf('name') !== -1)
-			type.name = req.body.name;
-		if (typeKeys.indexOf('colorId') !== -1)
-			type.colorId = req.body.colorId;
-		await Users.updateOne({ _id: req.user._id, "types.id": typeId }, {
-			$set: {
-				"types.$.colorId": type.colorId,
-				"types.$.name": type.name,
+		let types = await Users.findOne({_id: req.user._id}, {'types': {$elemMatch: {'id': typeId}}});
+		if (!types.types[0])
+			return res.status(400).send('id not found');
+		else {
+			let type = types.types[0];
+			let typeKeys = Object.keys(req.body);
+			if (typeKeys.indexOf('name') !== -1) {
+				if (typeof req.body.name !== "string")
+					return res.status(400).send('Wrong typeof name')
+				else
+					type.name = req.body.name;
 			}
-		},
-			(err, result) => {
-				if (err)
-					res.status(500).send('Error');
-				else if (!result)
-					res.status(400).send('Type not found');
-			})
-		res.status(200).send('success');
+			if (typeKeys.indexOf('name') === -1)
+				return res.status(400).send('Wrong param');
+			await Users.updateOne({_id: req.user._id, "types.id": typeId}, {
+					$set: {
+						"types.$.colorId": type.colorId,
+						"types.$.name": type.name,
+					}
+				},
+				(err, result) => {
+					if (err)
+						res.status(500).send('Error');
+					else
+						res.status(200).send('Success');
+				})
+		}
 	}
 }));
 
